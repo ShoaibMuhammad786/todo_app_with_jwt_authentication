3114a73d6fbe2a786b7123b297f24524fec6ad84
Shoaib Muhammad <smshoaib2001@gmail.com>
2025-09-06 20:14:07 +0500

    made a change in the token expiry

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
-    expiresIn: process.env.JWT_EXPIRE,
+    expiresIn: process.env.JWT_EXPIRY,
   });
 };
 

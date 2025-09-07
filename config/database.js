47a89b9894c1d9e80a6f5885a45b02b559250b92
Shoaib Muhammad <smshoaib2001@gmail.com>
2025-09-07 11:10:22 +0500

    changed the token expiry

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
   try {
-    const connections = await mongoose.connect(process.env.DB_URI);
+    await mongoose.connect(process.env.DB_URI);
     console.log(`Database connected`);
   } catch (error) {
     console.log(`Error while connecting database >>>>`, error.message);
